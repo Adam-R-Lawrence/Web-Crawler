30c88b56bc5f55e6ac06bea31dad6b3adf5cdb48
Adam Lawrence <arlawrence@student.unimelb.edu.au>
2020-03-26 20:02:53 +1100

    second commit

--- crawler.c
+++ crawler.c
@@ -8,98 +8,314 @@
 #include "crawler.h"
 #include <regex.h>
 
+queueForURLs *bottomStackPointer = NULL;
+queueForURLs *topStackPointer = NULL;
+int totalURLs = 0;
+
 
 int main(int argc,char *argv[]) {
 
     /*
     if (argc == 1){
-        printf("No URL was given\n");
+        printf("No URL was given\numberOfBytesRead");
         exit(EXIT_FAILURE);
     }*/
-    int socketfd = 0;
+
+    int socketfd;
     char sendBuff[1025] = {0};
     struct sockaddr_in serv_addr;
-    char recvBuff[1024] = {0};
-    //int port_number = 80;
+    char recvBuff[30000] = {0};
     char host[1024] = "www.columbia.edu";
     char url[1024] = "/~fdc/sample.html";
     struct hostent *server;
 
+/*
+    push(host);
+    push(host);
+    push("Bye");
+    enqueueURL("Hello");
+    printStack();
+*/
 
-
-
-    socketfd = socket(AF_INET, SOCK_STREAM, 0); //create socket
-    if(socketfd < 0) {
-        printf("ERROR opening socket\n");
-        exit(EXIT_FAILURE);
-    } else {
-        printf("Socket opened successfully.\n");
+    //Check if given URL is valid
+    if (checkIfValidURL(host) == IS_VALID){
+        enqueueURL(host);
     }
 
-    memset(&serv_addr, '0', sizeof(serv_addr)); //initialise server address
-    memset(sendBuff, '0', sizeof(sendBuff)); //initialise send buffer
+    printStack();
 
-    server = gethostbyname(host);
+    int numberOfBytesRead;
+    int total = 0;
+    int isheader = 0;
+    char *tail;
+    char * ahrefTail;
+    int index;
+    char *contentLengthHeader;
+    int contentLength = -2, cli;
+    int numberOfPagesFetched = 0;
+    char URL[1000];
 
-    //Create a Socket
-    serv_addr.sin_family = AF_INET; //Type of address – internet IP
-    serv_addr.sin_port = htons(PORT); //Listen on port 80
-    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+    while(numberOfPagesFetched < 1 && bottomStackPointer != NULL) {
 
-    //Connect
-    if(connect(socketfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("Can't connect\n");
-        exit(EXIT_FAILURE);
-    } else {
-        printf("Connected successfully\n");
+        numberOfPagesFetched++;
+        //dequeueURL(URL);
+
+        //Create the socket
+        socketfd = socket(AF_INET, SOCK_STREAM, 0);
+        if(socketfd < NO_SOCKET_OPENED) {
+            printf("Error with opening socket\n");
+            exit(EXIT_FAILURE);
+        } else {
+            printf("Socket opened successfully.\n");
+        }
+
+        memset(&serv_addr, '0', sizeof(serv_addr)); //initialise server address
+        memset(sendBuff, '0', sizeof(sendBuff)); //initialise send buffer
+
+
+        //Details of the Server
+        server = gethostbyname(host);
+        serv_addr.sin_family = AF_INET;
+        serv_addr.sin_port = htons(PORT);
+        memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+
+        //Connect to the desired Server
+        if(connect(socketfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+            printf("Error with connecting\n");
+            exit(EXIT_FAILURE);
+        } else {
+            printf("Connected successfully\n");
+        }
+
+        //Send HTTP GET request to the server
+        sprintf(sendBuff, HTTP_REQUEST_HEADER, url, host);
+        if(send(socketfd, sendBuff, strlen(sendBuff), 0) < 0) {
+            printf("Error with sending GET request\n");
+            exit(EXIT_FAILURE);
+        } else {
+            printf("Successfully sent html fetch request\n");
+        }
+
+        //Receive the response from the server
+        while (contentLength != total && (numberOfBytesRead = recv(socketfd, recvBuff, sizeof(recvBuff) - 1, 0)) > 0) {
+
+
+            if (isheader == 0) {
+                tail = strstr(recvBuff, END_OF_HTTP_HEADER) + 4;
+                contentLengthHeader = strstr(recvBuff, CONTENT_LENGTH) + strlen(CONTENT_LENGTH);
+                cli = (contentLengthHeader ? contentLengthHeader - recvBuff : -1);
+                contentLength = atoi(&(recvBuff[cli]));
+                printf("\n\n\n%d\n\n\n", contentLength);
+                //printf("\n\n\n%s\n\n\n", recvBuff);
+
+                if (tail != NULL) {
+                    //tail = tail -2;
+                    isheader = 1;
+                    index = (tail ? tail - recvBuff : -1);
+
+                    total += numberOfBytesRead - index;
+
+                    //printf ("%s", &(recvBuff[index]));
+                    //memset(recvBuff,0,strlen(recvBuff));
+                    continue;
+                }
+            }
+
+
+            /*
+            ahrefTail = strstr(recvBuff, "<a href=") + 8;
+            if( ahrefTail != NULL) {
+                ali = (ahrefTail ? ahrefTail - recvBuff : -1);
+                printf("\n\n\n\n%c\n\n\n\n", recvBuff[ali]);
+                //strncpy(substr, recvBuff[ali], 100);
+            }
+            */
+
+
+
+            parseHTML(recvBuff);
+            //printf("%s", recvBuff);
+
+            total += numberOfBytesRead;
+            memset(recvBuff, 0, strlen(recvBuff));
+
+        }
     }
+    printf("\n\n\n\n%d,%d\n\n\n\n",total,totalURLs);
 
-    //Send HTTP GET request
-    sprintf(sendBuff, HTTP_REQUEST_HEADER, url, host);
-
-
-    if(send(socketfd, sendBuff, strlen(sendBuff), 0) < 0) {
-        printf("Error with send()");
-    } else {
-        printf("Successfully sent html fetch request\n");
-    }
-
-
-    int n;
-    while ( (n = recv(socketfd, recvBuff, sizeof(recvBuff) - 1 ,0) ) > 0)
-    {
-            //process received buffer
-    printf("%s", recvBuff);
-    bzero(recvBuff, 1025);
-
-    }
-
-
+    printStack();
     return 0;
 }
 
 void fetchWebPage(char URL[], int numberOfPagesFetched)
 {
-    if (numberOfPagesFetched >= MAX_NUMBER_OF_PAGES_FETCHED){
-        exit(EXIT_SUCCESS);
-    } else {
-        numberOfPagesFetched++;
+    char possibleURL[1024];
+
+
+
+
+    if(checkIfValidURL(possibleURL) == IS_VALID){
+        enqueueURL(possibleURL);
     }
 
 
 }
 
-void parseHTTPHeaders(int socketFD)
+int parseHTTPHeaders(char buffer[]) {
+
+
+    return 1;
+}
+
+void parseHTML(char buffer[])
 {
+    int si, ei, URLLength;
+    char * startURL;
+    char * endURL;
+    char possibleURL[MAX_URL_SIZE + 1];
+
+
+
+    while ((startURL = strstr(buffer, "<a href=")) != NULL) {
+        startURL = &(startURL[9]);
+        si = (startURL ? startURL - buffer : -1);
+       // printf("Start: %c\n", buffer[si]);
+        //printf("\n\n\n\n%s\n\n\n\n", startURL);
+
+        endURL = strstr(startURL, "\"");
+        ei = (endURL ? endURL - buffer : -1);
+        //printf("End: %c\n", buffer[ei]);
+
+       // printf("URL: ");
+
+        URLLength = ei - si;
+        //printf("%d\n",URLLength);
+
+        for (int i = 0; i < URLLength; i++) {
+           // printf("%c",buffer[si + i]);
+        }
+        memcpy(possibleURL, &buffer[si], URLLength);
+        possibleURL[URLLength] = '\0';
+
+        if(checkIfValidURL(possibleURL) == IS_VALID){
+            enqueueURL(possibleURL);
+        }
+        //enqueueURL(possibleURL);
+
+        memset(possibleURL, 0, strlen(possibleURL));
+
+
+
+        totalURLs++;
+        printf("\n");
+       // memcpy(possibleURL, &buffer[si], URLLength);
+        //possibleURL[URLLength] = '\0';
+        //printf("URL: %s\n",possibleURL);
+        //memset(possibleURL, 0, strlen(possibleURL));
+
+        //strncpy(substr, recvBuff[ali], 100);
+
+
+        buffer = endURL;
+    }
+    //printf("\n\n\n\nHello\n\n\n");
 
 }
 
-void parseHTML(int socketFD)
+int checkIfValidURL(char possibleURL[])
 {
+    int validURL;
+
+    /* A valid URL's host should match the host of the given command line URL
+     *
+     *
+     */
+
+
+    //A valid URL cannot be over 1000 bytes long
+    if (strlen(possibleURL) > 1000)
+    {
+        return validURL = NOT_VALID;
+    }
+
+    //A valid URL contains no Exclamation Mark
+    if (strchr(possibleURL, EXCLAMATION_MARK) != NULL)
+    {
+        return validURL = NOT_VALID;
+    }
+
+    //A valid URL contains no Hashtag
+    if (strchr(possibleURL, HASHTAG) != NULL)
+    {
+        return validURL = NOT_VALID;
+    }
+
+    //A valid URL contains no URL Encoded Characters
+    if (strchr(possibleURL, URL_ENCODED_CHARACTER) != NULL)
+    {
+        return validURL = NOT_VALID;
+    }
+
+
+
+    return validURL = IS_VALID;
+}
+
+/*
+ *  A function that returns the host of a given URL
+ */
+char * getHost(char URL[]){
+
+    return "Hello";
+}
+
+void dequeueURL(char *URL){
+    if(bottomStackPointer == NULL){
+        return;
+    }
+    strcpy(URL,bottomStackPointer->data);
+    queueForURLs *TempPointer = bottomStackPointer;
+    bottomStackPointer = bottomStackPointer->nextNode;
+    free(TempPointer);
+
+    if(bottomStackPointer == NULL){
+        topStackPointer = NULL;
+    }
 
 }
 
-void checkIfValidURL(char URL[])
-{
+void enqueueURL(char *URL){
+    queueForURLs *TempPointer = malloc(sizeof(queueForURLs));
 
+    if(TempPointer == NULL){
+        return;
+    }
+    strcpy(TempPointer->data,URL);
+    TempPointer->nextNode = NULL;
+
+    if(bottomStackPointer == NULL)
+    {
+        TempPointer = bottomStackPointer = TempPointer;
+    }
+    else
+    {
+        topStackPointer->nextNode = TempPointer;
+    }
+    topStackPointer = TempPointer;
+
+}
+
+void printStack(void){
+    printf("***List of URL's***\n");
+    printf("**************\n");
+    int numberOfNodes = 0;
+    queueForURLs *TempPointer = bottomStackPointer;
+    while(TempPointer != NULL){
+        numberOfNodes++;
+        printf("%s\n",TempPointer->data);
+        TempPointer = TempPointer->nextNode;
+    };
+    printf("**************\n");
+    printf("Counted %d URL's\n",numberOfNodes);
+    printf("***List of URL's***\n");
 }
--- crawler.h
+++ crawler.h
@@ -8,15 +8,31 @@
 /* Defines */
 #define PORT 80
 #define HTTP_REQUEST_HEADER "GET /%s HTTP/1.1\r\nHost: %s\r\nUser-Agent: arlawrence\r\n\r\n"
-#define MAX_URL_LENGTH 1000
 #define MAX_NUMBER_OF_PAGES_FETCHED 100
+#define IS_VALID 1
+#define NOT_VALID 0
+#define EXCLAMATION_MARK '!'
+#define HASHTAG '#'
+#define URL_ENCODED_CHARACTER '%'
+#define MAX_URL_SIZE 1000
+#define END_OF_HTTP_HEADER "\r\n\r\n"
+#define CONTENT_LENGTH "Content-Length: "
+#define NO_SOCKET_OPENED 0
+
+
+typedef struct queueForURLs{
+    char data[MAX_URL_SIZE];
+    struct queueForURLs *nextNode;
+} queueForURLs;
 
 /* Function Prototypes */
 void fetchWebPage(char URL[], int numberOfPagesFetched);
-void parseHTTPHeaders(int socketFD);
-void parseHTML(int socketFD);
-void checkIfValidURL(char URL[]);
-
+int parseHTTPHeaders(char *buffer);
+void parseHTML(char buffer[]);
+int checkIfValidURL(char possibleURL[]);
+void enqueueURL(char *URL);
+void printStack(void);
+void dequeueURL(char *URL);
 
 
 
